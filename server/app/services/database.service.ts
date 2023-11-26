6593d2ec55e9df034866364ef26a31b385fef424
lillo-lec <lillo-lec@outlook.com>
2023-11-25 20:39:39 -0500

    fonctions Server-BdD: add,delete,insert

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -6,7 +6,7 @@ import "reflect-metadata";
 export class DatabaseService {
   public connectionConfig: pg.ConnectionConfig = {
     user: "postgres",
-    database: "TP4",
+    database: "hopital_bd",
     password: "root",
     port: 5432,          // Attention ! Peut aussi être 5433 pour certains utilisateurs
     host: "127.0.0.1",
@@ -14,4 +14,28 @@ export class DatabaseService {
   };
 
   public pool: pg.Pool = new pg.Pool(this.connectionConfig);
+
+  public async getAllMedecin(): Promise<pg.QueryResult> {
+    const client = await this.pool.connect();
+    const res = await client.query("SELECT * FROM hopital_bd.Medecins;");
+    client.release()
+    return res;
+  }
+
+  public async addMedecin(idMedecin: number, prenom: string, nom: string, specialite: string, anneesExperience: number, idService: number): Promise<pg.QueryResult> {
+    const client = await this.pool.connect();
+    const query = 'INSERT INTO hopital_bd.Medecins VALUES($1, $2, $3, $4, $5, $6)';
+    const values = [idMedecin, prenom, nom, specialite, anneesExperience, idService];
+    const res = await client.query(query, values);
+    client.release()
+    return res;
+  }
+
+  public async deleteMedecin(idMedecin: number): Promise<pg.QueryResult> {
+    const client = await this.pool.connect();
+    const query = `DELETE FROM hopital_bd.Medecins WHERE idMedecin='${idMedecin}'`;
+    const res = await client.query(query);
+    client.release()
+    return res;
+  }
 }
--- server/out/services/database.service.js
+++ server/out/services/database.service.js
@@ -5,6 +5,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DatabaseService = void 0;
 const inversify_1 = require("inversify");
@@ -14,13 +23,40 @@ let DatabaseService = class DatabaseService {
     constructor() {
         this.connectionConfig = {
             user: "postgres",
-            database: "TP4",
+            database: "hopital_bd",
             password: "root",
             port: 5432,
             host: "127.0.0.1",
             keepAlive: true
         };
         this.pool = new pg.Pool(this.connectionConfig);
+    }
+    getAllMedecin() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const client = yield this.pool.connect();
+            const res = yield client.query("SELECT * FROM hopital_bd.Medecins;");
+            client.release();
+            return res;
+        });
+    }
+    addMedecin(idMedecin, prenom, nom, specialite, anneesExperience, idService) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const client = yield this.pool.connect();
+            const query = 'INSERT INTO hopital_bd.Medecins VALUES($1, $2, $3, $4, $5, $6)';
+            const values = [idMedecin, prenom, nom, specialite, anneesExperience, idService];
+            const res = yield client.query(query, values);
+            client.release();
+            return res;
+        });
+    }
+    deleteMedecin(idMedecin) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const client = yield this.pool.connect();
+            const query = `DELETE FROM hopital_bd.Medecins WHERE idMedecin='${idMedecin}'`;
+            const res = yield client.query(query);
+            client.release();
+            return res;
+        });
     }
 };
 DatabaseService = __decorate([
